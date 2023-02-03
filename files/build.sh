50ec43c9aa67a33386ab1db81ee00c31364dbe66
Alex Kirhenshtein <alk@netxms.org>
2023-02-03 17:28:47 +0200

    mock config moved to separate file

--- files/build.sh
+++ files/build.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-#cat > /etc/mock/site-defaults.cfg <<__END
-#config_opts['plugin_conf']['tmpfs_enable'] = True
-#config_opts['plugin_conf']['tmpfs_opts'] = {}
-#config_opts['plugin_conf']['tmpfs_opts']['required_ram_mb'] = 1024
-#config_opts['plugin_conf']['tmpfs_opts']['max_fs_size'] = '5g'
-#config_opts['plugin_conf']['tmpfs_opts']['mode'] = '0755'
-#config_opts['plugin_conf']['tmpfs_opts']['keep_mounted'] = False
-#
-#config_opts['plugin_conf']['bind_mount_enable'] = True
-#config_opts['plugin_conf']['bind_mount_opts']['dirs'].append(('/var/cache/mock/m2-repo', '/m2-repo' ))
-#__END
-
 mkdir -p /var/cache/mock/m2-repo
 
 for V in 8 9; do
