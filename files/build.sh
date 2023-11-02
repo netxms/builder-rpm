0a07957651b9e5b61a0ee45852a9ccbdbfcfe2ef
Alex Kirhenshtein <alk@netxms.org>
2023-11-02 12:35:51 +0200

    mock log files are dumped to stdout if build failed

--- files/build.sh
+++ files/build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+function dump_logs_and_exit() {
+    original_exit_code=$?
+    set +e
+    shopt -s dotglob nullglob
+    for f in /var/lib/mock/*/result/*.log; do
+        echo
+        echo
+        echo "### LOG: $f"
+        cat $f
+        echo
+        echo "### End of $f"
+    done
+    exit $original_exit_code
+}
+
 set -e
 
 cd /drone/src
@@ -9,13 +24,15 @@ mkdir -p /var/cache/mock/m2-repo
 for V in 8 9; do
    mock --enable-network -r rocky+epel-$V-$(arch) --spec SPECS/*.spec --sources SOURCES \
       --addrepo https://packages.netxms.org/devel/epel/$V/$(arch)/stable \
-      --addrepo https://packages.netxms.org/epel/$V/$(arch)/stable
+      --addrepo https://packages.netxms.org/epel/$V/$(arch)/stable \
+      || dump_logs_and_exit
 done
 
 for V in 37 38; do
    mock --enable-network -r fedora-$V-$(arch) --spec SPECS/*.spec --sources SOURCES \
       --addrepo https://packages.netxms.org/devel/fedora/$V/$(arch)/stable \
-      --addrepo https://packages.netxms.org/fedora/$V/$(arch)/stable
+      --addrepo https://packages.netxms.org/fedora/$V/$(arch)/stable \
+      || dump_logs_and_exit
 done
 
 cp /var/lib/mock/*/result/*.rpm /result/
